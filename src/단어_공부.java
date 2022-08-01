25560077937d8837be2e40ad226b0da6b51db5f7
CordHouse <qkqktl5310@gmail.com>
2022-08-01 18:45:52 +0900

    docs : Add 주석

--- src/단어_공부.java
+++ src/단어_공부.java
@@ -5,11 +5,14 @@ import java.util.*;
 
 public class 단어_공부 {
     public static void main(String[] args) throws IOException {
+        // Step 1. 변수 선언
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String inputStr = br.readLine().toUpperCase();
-
+        // map -> 문자를 담아주기 위해 선언
         Map<Character, Integer> map = new HashMap<>();
 
+        // Step 2. 반복
+        // 입력받은 문자열을 char형태로 key : value로 넣어준다.
         for(int i = 0; i < inputStr.length(); i++){
             char key = inputStr.charAt(i);
             if(!map.containsKey(key))
@@ -18,6 +21,7 @@ public class 단어_공부 {
                 map.put(key, map.get(key)+1);
         }
 
+        // Step 3. key를 List로 만들어 value를 기준으로 정렬한다.
         List<Character> list = new LinkedList<>(map.keySet());
         Collections.sort(list, new Comparator<Character>() {
             @Override
