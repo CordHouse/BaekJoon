7b632b1e224cb168445eadbbf16af479e2f8568a
CordHouse <qkqktl5310@gmail.com>
2022-08-31 03:14:25 +0900

    22.08.31

--- src/RGB거리.java
+++ src/RGB거리.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class test {
+public class RGB거리 {
     public static void main(String[] args) throws IOException {
         // Step 1. 변수 선언
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
