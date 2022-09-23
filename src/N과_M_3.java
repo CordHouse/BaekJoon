f6b63e44f0d14ce64bb56e240e880ab5b1461cc8
CordHouse <qkqktl5310@gmail.com>
2022-09-23 15:59:06 +0900

    22.09.23

--- src/N과_M_3.java
+++ src/N과_M_3.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class N과_M_3 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N과_M_3 {
+    static int N, M;
+    static int[] ans;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        ans = new int[M];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth){
+        if(depth == M){
+            for(int value : ans){
+                sb.append(value).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 1; i<= N; i++){
+            ans[depth] = i;
+            dfs(depth+1);
+        }
+    }
 }
--- src/N과_M_4.java
+++ src/N과_M_4.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class N과_M_4 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N과_M_4 {
+    static int N, M;
+    static int[] ans;
+    static boolean[] check;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        ans = new int[M];
+        check = new boolean[N+1];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth){
+        if(depth == M){
+            for(int value : ans){
+                sb.append(value).append(" ");
+            }
+            sb.append("\n");
+            check[ans[0]] = true;
+            return;
+        }
+
+        for(int i = 1; i<=N; i++){
+            if(!check[i]) {
+                ans[depth] = i;
+                if(depth >= 1 && ans[depth-1] > ans[depth]){
+                    continue;
+                }
+                dfs(depth + 1);
+            }
+        }
+    }
 }
