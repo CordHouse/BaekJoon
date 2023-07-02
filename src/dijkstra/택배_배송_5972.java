b8db4c61970b0da41ced6bd8aede1173fc73df68
CordHouse <qkqktl5310@gmail.com>
2023-07-02 13:56:43 +0900

    2023.07.02 택배 배송 (주석추가)

--- src/dijkstra/택배_배송_5972.java
+++ src/dijkstra/택배_배송_5972.java
@@ -42,16 +42,22 @@ public class 택배_배송_5972 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
+
+        // Step 1. 변수 선언 및 초기화
         n = Integer.parseInt(input[0]);
         m = Integer.parseInt(input[1]);
-        load = new int[n+1];
-        Arrays.fill(load, Integer.MAX_VALUE);
+        load = new int[n+1]; // 각 길 마다의 가중치를 갱신할 변수
+        Arrays.fill(load, Integer.MAX_VALUE); // 최소 값을 구해야하기 때문에 처음엔 맥스로 채워준다.
 
+        // Step 2. 노드 정보를 담을 변수 초기화
         root = new ArrayList[n+1];
         for(int i = 1; i <= n; i++) {
             root[i] = new ArrayList<>();
         }
 
+        // Step 3. 입력 값 대입
+        // 양방향으로 이어져있이 때문에 cur, next 두 개다 값을 넣어줘야함
+        // 단방향이면 하나만 해줘야한다.
         for(int i = 0; i < m; i++) {
             String[] line = br.readLine().split(" ");
             int cur = Integer.parseInt(line[0]);
@@ -62,6 +68,7 @@ public class 택배_배송_5972 {
             root[next].add(new Node(cur, value));
         }
 
+        // Step 4. 다익스트라 시작, 방문처리 배열도 함께 초기화
         check = new boolean[n+1];
         dijkstra(1);
         System.out.println(load[n]);
@@ -69,8 +76,8 @@ public class 택배_배송_5972 {
 
     public static void dijkstra(int start) {
         PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> o1.value - o2.value);
-        queue.add(new Node(start, 0));
-        load[start] = 0;
+        queue.add(new Node(start, 0)); // 시작 되는 값은 1
+        load[start] = 0; // 처음 위치는 거리가 0
 
         while(!queue.isEmpty()) {
             Node node = queue.poll();
@@ -80,7 +87,8 @@ public class 택배_배송_5972 {
             }
 
             for(Node nextNode : root[node.next]) {
-                if(!check[nextNode.next] && load[nextNode.next] >= node.value + nextNode.value) {
+                // 처음과 다음 가중치를 합한게 기존에 담아놓은 가중치보다 작아야지 갱신 가능능
+               if(!check[nextNode.next] && load[nextNode.next] >= node.value + nextNode.value) {
                     load[nextNode.next] = node.value + nextNode.value;
                     queue.add(new Node(nextNode.next, load[nextNode.next]));
                 }
