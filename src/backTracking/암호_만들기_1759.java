8b16838cb687cf5c1c38b9de4c0b055f195c26d9
CordHouse <qkqktl5310@gmail.com>
2023-08-23 15:54:20 +0900

    2023.08.23 암호 만들기

--- src/backTracking/암호_만들기_1759.java
+++ src/backTracking/암호_만들기_1759.java
@@ -1,11 +1,32 @@
 package backTracking;
 
+/**
+ * 23. 08. 23 15시 05분 시작
+ * 23. 08. 23 15시 47분 종료
+ * 성공 유무 -> 성공
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class 암호_만들기 {
+/**
+ * 주어진 조건
+ * 1. 알파벳의 모음은 최소 1개 자음은 최소 2개로 구성되어 있다.
+ * 2. 암호는 항상 순서를 갖는 것만 암호가 될 수 있다. abc (o) bac (x)
+ * 3. 중복은 존재하지 않는다.
+ *
+ * 제한사항
+ * 3 <= L <= C <= 15
+ *
+ * 풀이
+ * 주어진 조건에 따라 사전순으로 정렬된 암호만이 암호가 될 수 있기 때문에 주어진 알파벳을 미리 정렬해준다.
+ * 그 후 정렬된 알파벳을 기반으로 모음 1개, 자음 2개 이상으로 구성된 문자를 찾아준다.
+ * L개의 문자를 만든 후 값을 출력한다.
+ */
+
+public class 암호_만들기_1759 {
     private static String[] password;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
