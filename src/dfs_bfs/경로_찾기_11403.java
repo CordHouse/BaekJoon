83dbf186a66d984576bb7cae2b66219b5ddacdcd
CordHouse <qkqktl5310@gmail.com>
2024-03-21 01:57:47 +0900

    2024.03.20 경로_찾기_11403

---
+++ src/dfs_bfs/경로_찾기_11403.java
@@ -0,0 +1,39 @@
+package dfs_bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 경로_찾기_11403 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[][] inputTable = new String[n][n];
+
+        for(int i = 0; i < n; i++) {
+            String[] splitValue = br.readLine().split(" ");
+            for(int j = 0; j < n; j++) {
+                inputTable[i][j] = splitValue[j];
+            }
+        }
+
+        for(int k = 0; k < n; k++) {
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(inputTable[i][k].equals("1") && inputTable[k][j].equals("1")) {
+                        inputTable[i][j] = "1";
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                sb.append(inputTable[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
